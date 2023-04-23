0d545d6cb4b0f68035b112867aeaf845a609129f
Abolfazl Tabe Bordbar <77238230+Abolfazl-Tabe-Bordbar@users.noreply.github.com>
2023-04-23 22:55:24 +0430

    add all

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+    <Body class="w-screen h-screen bg-green-500 flex justify-center items-center"></Body>
+    <span class="font-bold text-lg">Tailwindcss Configure Success</span>
+</template>
+
+<style scoped></style>
